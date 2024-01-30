c9c3764d0898894c68ebf79c179b1e84cf20d33b
SHakil Hossain <shakil@newgen-bd.com>
2024-01-31 02:07:27 +0600

    Update index.blade.php

--- resources/views/document/index.blade.php
+++ resources/views/document/index.blade.php
@@ -5,105 +5,119 @@
 @endsection
 
 @section('content')
-<div class="container-fluid my-5 py-5" style="background: white;">
-    <div class="row">
-        <div style="display: flex; justify-content: space-between; align-items: center;">
-            <span id="card_title">
-                {{ __('All Documents') }}
-            </span>
-            
-            @if (auth()->check() && auth()->user()->role && auth()->user()->role->name == 'superadmin')
-                <div class="float-right">
-                    <a href="{{ route('document.create') }}" class="btn btn-primary btn-sm float-right" data-placement="left">
-                        {{ __('Create New') }}
-                    </a>
-                </div>
-            @endif
-        </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card bg-white">
+                    <div class="card-header">
+                        <div style="display: flex; justify-content: space-between; align-items: center;">
 
-        @if ($message = Session::get('success'))
-            <div class="alert alert-success m-3" id="successAlert">
-                <p>{{ $message }}</p>
-            </div>
-        @endif
-
-        <div class="card-body">
-            <div class="table-responsive">
-                <form method="get" action="{{ route('document.index') }}">
-                    <div class="input-group mb-3">
-                        <input type="text" name="search" value="{{ $search }}" class="form-control"
-                               placeholder="Search by title, tag, status, file path, or description" aria-label="Search" aria-describedby="basic-addon2">
-                        <button type="submit" class="btn btn-primary">Search</button>
+                            <span id="card_title">
+                                {{ __('All Documents') }}
+                            </span>
+                            @if (auth()->check() && auth()->user()->role && auth()->user()->role->name == 'superadmin')
+                                <div class="float-right">
+                                    <a href="{{ route('document.create') }}" class="btn btn-primary btn-sm float-right"
+                                        data-placement="left">
+                                        {{ __('Create New') }}
+                                    </a>
+                                </div>
+                            @endif
+                        </div>
                     </div>
-                </form>
+                    @if ($message = Session::get('success'))
+                        <div class="alert alert-success m-3" id="successAlert">
+                            <p>{{ $message }}</p>
+                        </div>
+                        <script>
+                            setTimeout(function() {
+                                document.getElementById('successAlert').style.display = 'none';
+                            }, 10000);
+                        </script>
+                    @endif
 
-                <table class="table table-striped table-hover">
-                    <thead class="thead">
-                        <tr>
-                            <th>No</th>
-                            <th>Title</th>
-                            <th>Tag</th>
-                            <th>Onlyuser</th>
-                            <th>Status</th>
-                            <th>File Name</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($documents as $document)
-                            <tr>
-                                <td>{{ ++$i }}</td>
-                                <td>{{ $document->title }}</td>
-                                <td>{{ $document->tag }}</td>
-                                <td>{{ $document->user->name }}</td>
-                                <td>{{ $document->status }}</td>
-                                <td>{{ basename($document->file_path) }}</td>
-                                {{-- <td>{{ $document->description }}</td> --}}
-                                <td>
-                                    @if (auth()->check() && auth()->user()->role && auth()->user()->role->name == 'superadmin')
-                                        <div class="btn-group" role="group" aria-label="Document Actions">
-                                            <a class="btn btn-sm btn-primary" href="{{ route('document.show', $document->id) }}">
-                                                <i class="fa fa-fw fa-eye"></i> {{ __('Show') }}
-                                            </a>
-                                        
-                                            <a class="btn btn-sm btn-success" href="{{ route('document.edit', $document->id) }}">
-                                                <i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}
-                                            </a>
-                                        
-                                            <form action="{{ route('document.destroy', $document->id) }}" method="POST" style="display: inline;">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this document?')">
-                                                    <i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}
-                                                </button>
-                                            </form>
-                                        </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <div class="container mt-3">
+                                <form method="get" action="{{ route('document.index') }}">
+                                    <div class="input-group mb-3">
+                                        <input type="text" name="search" value="{{ $search }}" class="form-control"
+                                               placeholder="Search by title, tag, status, file path, or description" aria-label="Search" aria-describedby="basic-addon2">
+                                        <button type="submit" class="btn btn-primary">Search</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <table class="table table-striped table-hover">
+                                <thead class="thead">
+                                    <tr>
+                                        <th>No</th>
+
+                                        <th>Title</th>
+                                        <th>Tag</th>
+                                        <th>Onlyuser</th>
+                                        <th>Status</th>
+                                        <th>File Name</th>                                        
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @if (count($documents) > 0)
+                                        @foreach ($documents as $document)
+                                            <tr>
+                                                <td>{{ ++$i }}</td>
+
+                                                <td>{{ $document->title }}</td>
+                                                <td>{{ $document->tag }}</td>
+                                                <td>{{ $document->user->name }}</td>
+                                                <td>{{ $document->status }}</td>
+                                                <td>{{ basename($document->file_path) }}</td>
+                                                {{-- <td>{{ $document->description }}</td> --}}
+
+                                                <td>
+                                                    
+                                                    
+                                                        @if (auth()->check() && auth()->user()->role && auth()->user()->role->name == 'superadmin')
+                                                        <div class="btn-group" role="group" aria-label="Document Actions">
+                                                            <a class="btn btn-sm btn-primary" href="{{ route('document.show', $document->id) }}">
+                                                                <i class="fa fa-fw fa-eye"></i> {{ __('Show') }}
+                                                            </a>
+                                                        
+                                                            <a class="btn btn-sm btn-success" href="{{ route('document.edit', $document->id) }}">
+                                                                <i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}
+                                                            </a>
+                                                        
+                                                            <form action="{{ route('document.destroy', $document->id) }}" method="POST" style="display: inline;">
+                                                                @csrf
+                                                                @method('DELETE')
+                                                        
+                                                                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this document?')">
+                                                                    <i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}
+                                                                </button>
+                                                            </form>
+                                                        </div>
+                                                        
+                                                        @else
+                                                            <a class="btn btn-sm btn-primary" href="{{ route('document.show', $document->id) }}">
+                                                                <i class="fa fa-fw fa-eye"></i> {{ __('Show') }}
+                                                            </a>
+                                                        @endif
+                                                    
+                                                </td>
+                                            </tr>
+                                        @endforeach
                                     @else
-                                        <a class="btn btn-sm btn-primary" href="{{ route('document.show', $document->id) }}">
-                                            <i class="fa fa-fw fa-eye"></i> {{ __('Show') }}
-                                        </a>
+                                        <tr>
+                                            <td class="text-danger" colspan="8">No Data Found</td>
+                                        </tr>
                                     @endif
-                                </td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <td class="text-danger" colspan="7">No Data Found</td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                {!! $documents->links() !!}
             </div>
         </div>
-        
-        {!! $documents->links() !!}
     </div>
-</div>
-
-<!-- JavaScript to hide success alert after 10 seconds -->
-<script>
-    setTimeout(function() {
-        document.getElementById('successAlert').style.display = 'none';
-    }, 10000);
-</script>
-
 @endsection
