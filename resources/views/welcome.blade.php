1c1950a3e43a237085373c08c27515a30a1c163b
mdashraful <mdashrafulislam369@gmail.com>
2024-01-30 23:47:54 +0600

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,8 @@
         <div class="row">
             <h2 class="text-center">Welcome</h2>
         </div>
+        <div class="card">
+            <img src="{{asset('')}}" alt="">
+        </div>
     </div>
 @endsection
