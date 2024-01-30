11431b4375c960d948ec9d4a1bd37db994323b36
SHakil Hossain <shakil@newgen-bd.com>
2024-01-30 17:51:25 +0600

    update

--- resources/views/document/edit.blade.php
+++ resources/views/document/edit.blade.php
@@ -1,16 +1,31 @@
 @extends('layouts.app')
 
+@section('template_title')
+    {{ __('Update') }} Document
+@endsection
+
 @section('content')
-    <div class="container">
-        <div>
-            <h3 class="text-center">
-                Edit Document
-            </h3>
+    <section class="content container-fluid">
+        <div class="">
+            <div class="col-md-12">
+
+                @includeif('partials.errors')
+
+                <div class="card card-default">
+                    <div class="card-header">
+                        <span class="card-title">{{ __('Update') }} Document</span>
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('document.update', $document->id) }}"  role="form" method= 'POST'  enctype='multipart/form-data'>
+                            {{ method_field('PATCH') }}
+                            @csrf
+
+                            @include('document.form')
+
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <form action="{{ route('documents.update', $document->id) }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            @method('PATCH')
-            @include('document.form')
-        </form>
-    </div>
+    </section>
 @endsection
--- resources/views/document/form.blade.php
+++ resources/views/document/form.blade.php
@@ -1,36 +1,39 @@
-@isset($document->file_path)
-    @php
-        $file_path = $document->file_path ?? '';
-        $prefix = 'document/';
-        $file_name = str_replace($prefix, '', $file_path);
-    @endphp
-@endisset
+<div class="box box-info padding-1">
+    <div class="box-body">
+        
+        <div class="form-group">
+            {{ Form::label('title') }}
+            {{ Form::text('title', $document->title, ['class' => 'form-control' . ($errors->has('title') ? ' is-invalid' : ''), 'placeholder' => 'Title']) }}
+            {!! $errors->first('title', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('tag') }}
+            {{ Form::text('tag', $document->tag, ['class' => 'form-control' . ($errors->has('tag') ? ' is-invalid' : ''), 'placeholder' => 'Tag']) }}
+            {!! $errors->first('tag', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('onlyuser') }}
+            {{ Form::text('onlyuser', $document->onlyuser, ['class' => 'form-control' . ($errors->has('onlyuser') ? ' is-invalid' : ''), 'placeholder' => 'Onlyuser']) }}
+            {!! $errors->first('onlyuser', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('status') }}
+            {{ Form::text('status', $document->status, ['class' => 'form-control' . ($errors->has('status') ? ' is-invalid' : ''), 'placeholder' => 'Status']) }}
+            {!! $errors->first('status', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('file_path', 'File Path') }}
+            {{ Form::file('file_path', ['class' => 'form-control' . ($errors->has('file_path') ? ' is-invalid' : '')]) }}
+            {!! $errors->first('file_path', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('description') }}
+            {{ Form::text('description', $document->description, ['class' => 'form-control' . ($errors->has('description') ? ' is-invalid' : ''), 'placeholder' => 'Description']) }}
+            {!! $errors->first('description', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
 
-<div class="mb-3">
-    <label for="title" class="form-label">Title:</label>
-    <input type="text" name="title" class="form-control" id="title" placeholder="title here"
-        value="{{ isset($document->title) ? $document->title : old('title') }}">
-    @error('title')
-        <small class="text-danger d-block">{{ $message }}</small>
-    @enderror
-</div>
-
-<div class="mb-3">
-    <label class="form-label" for="doc_file">Upload:</label>
-    @isset($file_name)
-        <span class="text-success">
-            <a href="{{ asset('storage/' . $document['file_path']) }}" target="_blank">
-                <small>{{ $file_name ?? '' }}</small>
-            </a>
-        </span>
-    @endisset
-    <input type="file" name="doc_file" class="form-control" id="doc_file">
-    @error('doc_file')
-        <small class="text-danger d-block">{{ $message }}</small>
-    @enderror
-</div>
-
-<div class="d-flex gap-2">
-    <x-go-back />
-    <button class="btn btn-md btn-success">Submit</button>
+    </div>
+    <div class="box-footer mt20">
+        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+    </div>
 </div>
