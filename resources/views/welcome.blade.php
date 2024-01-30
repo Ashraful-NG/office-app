afa915ffd456e51caac6e197b4bf1d1b1c81055f
mdashraful <mdashrafulislam369@gmail.com>
2024-01-30 23:51:56 +0600

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,12 @@
         <div class="row">
             <h2 class="text-center">Welcome</h2>
         </div>
-        <div class="card">
-            <img src="{{asset('')}}" alt="">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <img src="{{ asset('img/stock-photo-folders.jpeg') }}" class="img-fluid" alt="Responsive image">
+                </div>
+            </div>
         </div>
     </div>
 @endsection
