0382cd74b93343814214d11d4a3e34d377c5db9e
SHakil Hossain <shakil@newgen-bd.com>
2024-01-31 11:55:36 +0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
@@ -13,23 +13,17 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
-    {{--
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"> --}}
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
-
     <link rel="icon" href="{{ asset('img/caab24.png') }}" type="image/png">
-    <!-- Scripts -->
+
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-
-
 
 </head>
 
@@ -53,146 +47,93 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    @if (auth()->check())
                     <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('document.index') }}">Documents</a>
-                        </li>
+                        @if (auth()->check())
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('document.index') }}">Documents</a>
+                            </li>
+                        @endif
                     </ul>
-                    @endif
 
-                    <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
                         @guest
-                        @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a class="btn btn-dark px-5" href="{{ route('login') }}">
-                                {{ __('Login') }}
-                            </a>
-                        </li>
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="btn btn-dark px-5" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
 
-                        @endif
-
-                        @if (Route::has('register'))
-                        <li class="nav-item d-none">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
+                            @if (Route::has('register'))
+                                <li class="nav-item d-none">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
                         @else
+                            <li>
+                                <a class="nav-link" href="#">{{ Auth::user()->name }}</a>
+                            </li>
 
-                        <li>
+                            <li class="nav-item dropdown">
+                                <a class=" btn btn-danger" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
 
-                            <!-- Logout Form -->
-
-
-                            <a class="nav-link " href="#">
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            {{-- <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <!-- User Profile Link or Additional Dropdown Items -->
-
-                                {{-- <a class="dropdown-item" href="{{ route('profile') }}">
-                                    {{ __('Profile') }}
-                                </a> --}}
-
-                                {{-- <div class="dropdown-divider"></div> --}}
-
-                                <!-- Logout Link -->
-                                {{--
-                            </div> --}}
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <a class=" btn btn-danger" href="{{ route('logout') }}"
-                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    class="d-none">
+                                    @csrf
+                                </form>
+                            </li>
                         @endguest
                     </ul>
                 </div>
             </div>
         </nav>
 
-
-
-        <div class="container" style="
-        height: 81vh;
-    ">
+        <div class="container" style="height: 100vh;">
             <div class="row d-flex align-items-center justify-content-center my-5">
-                <!-- Left Column -->
                 <div class="col-md-6">
-                    <h2 style="color: #022C22;
-                        font-family: Poppins;
-                        font-size: 32px;
-                        font-style: normal;
-                        font-weight: 700;
-                        line-height: normal;
-                        text-transform: capitalize;">
+                    <h2 style="color: #022C22; font-family: Poppins; font-size: 32px; font-style: normal; font-weight: 700;
+                        line-height: normal; text-transform: capitalize;">
                         Welcome to Document Management System (DMS)
                     </h2>
-                    <p>
-                        A document management system (DMS) is a software that helps to store, organize, track, and
+                    <p>A document management system (DMS) is a software that helps to store, organize, track, and
                         retrieve digital documents. A DMS can also provide features such as version control, access
                         control, search, and collaboration. A DMS can help to reduce paper usage, improve workflow,
                         enhance security, and comply with legal requirements.
                     </p>
-                    <a style="
-                    color:  #FFF;
-                    display: flex;
-height: 58px;
-padding: 16px 24px;
-justify-content: center;
-align-items: center;
-gap: 12px;
-border-radius: 18px;
-                    background: var(--color-emerald-950, #022C22);
-                    box-shadow: 0px 10px 20px 0px rgba(70, 16, 185, 0.30);
-                    width:150px
-                    
-                    " href="{{ route('login') }}">
-                        {{ __('Login') }}
-                    </a>
+                    <a style="color:  #FFF; display: flex; height: 58px; padding: 16px 24px; justify-content: center; align-items: center; gap: 12px; border-radius: 18px; background:  #022C22; box-shadow: 0px 10px 20px 0px rgba(70, 16, 185, 0.30); width:150px; text-decoration: none; font-weight: 800;"
+                        href="{{ route('login') }}">{{ __('Login') }}</a>
                 </div>
-
-                <!-- Right Column -->
                 <div class="col-md-6">
                     <img src="{{ asset('Hero.png') }}" width="100%" alt="CAAB" class="me-2">
                 </div>
             </div>
-
         </div>
-    </div>
-    <!-- Footer -->
-    <footer class="bg-black text-white py-3">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <p>&copy; 2024 Your Company Name</p>
-                </div>
-                <div class="col-md-6 text-end text-white">
-                    <ul class="list-inline">
-                        <li class="list-inline-item"><a href="#" style="color: #FFF; text-decoration: none;">Terms</a>
-                        </li>
-                        <li class="list-inline-item"><a href="#" style="color: #FFF; text-decoration: none;">Privacy</a>
-                        </li>
-                        <li class="list-inline-item"><a href="#" style="color: #FFF;  text-decoration: none;">Cookie
-                                Policy</a></li>
-                    </ul>
+
+        <!-- Footer -->
+        <footer class="bg-black text-white py-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>&copy; 2024 Your Company Name</p>
+                    </div>
+                    <div class="col-md-6 text-end text-white">
+                        <ul class="list-inline">
+                            <li class="list-inline-item"><a href="#" style="color: #FFF; text-decoration: none;">Terms</a>
+                            </li>
+                            <li class="list-inline-item"><a href="#" style="color: #FFF; text-decoration: none;">Privacy</a>
+                            </li>
+                            <li class="list-inline-item"><a href="#"
+                                    style="color: #FFF;  text-decoration: none;">Cookie Policy</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    </footer>
+        </footer>
+    </div>
 
     <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
-    {{-- <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script> --}}
     <script src="{{ asset('js/custom.js') }}"></script>
 </body>
 
