d8b2277350c74de6e2f8ffb30ae89efb12f196f4
SHakil Hossain <shakil@newgen-bd.com>
2024-01-31 10:29:47 +0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
         <div class="row">
             <h2 class="text-center">Welcome</h2>
         </div>
-        <div class="row justify-content-center bg-white">
-            <div class="col-md-8 shadow"> 
+        <div class="row justify-content-center ">
+            <div class="bg-white col-md-8 shadow p-5 my-5"> 
                <h2> Welcome to <br>
                 Document Management System (DMS)</h2>
 
