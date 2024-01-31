5ed02b2bac92c34e7e1df1389d39a461e67eb655
SHakil Hossain <shakil@newgen-bd.com>
2024-01-31 11:00:02 +0600

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Document;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        $data=Document::count();
+        return  view('home', compact('data'));
     }
 }
