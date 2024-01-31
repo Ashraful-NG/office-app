15afeb8766c66afa2cffbd4fe152537aeec1d17e
SHakil Hossain <shakil@newgen-bd.com>
2024-01-31 11:02:59 +0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-                <div class="card-body">
+        <div class="col-md-8 p-5 bg-white shadow rounded">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-                    {{ __('You are logged in!') }}
-                    @dd(  $data)
+                   
+                    <h1>Total Document  {{ $data}}</h1>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 @endsection
