76ecfdc0e58ba591897b02ae7b7798fc2b792468
SHakil Hossain <shakil@newgen-bd.com>
2024-01-31 11:23:22 +0600

    fg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,19 +4,20 @@
     <div class="container">
         
         
-            <div class="bg-white shadow p-5 my-5 rounded mx-auto " style="max-width: 600px">
+            <div class="bg-white shadow p-5 my-5 rounded mx-auto">
                 <div class="row ">
-            <div class="col-md-4">
-                 <img src="{{ asset('img/caab24.png') }}" width="150" alt="CAAB" class="me-2">
+                    <div class="col-md-6"> 
+                        <h2> Welcome to <br>
+                         Document Management System (DMS)</h2>
+        
+                         <a class="btn btn-danger btn-lg px-5 w-100" href="{{ route('login') }}">
+                            {{ __('Login') }}
+                        </a>
+                     </div>
+            <div class="col-md-6">
+                 <img src="{{ asset('Hero.png') }}" width="150" alt="CAAB" class="me-2">
             </div>
-            <div class="col-md-8"> 
-                <h2> Welcome to <br>
-                 Document Management System (DMS)</h2>
-
-                 <a class="btn btn-danger btn-lg px-5 w-100" href="{{ route('login') }}">
-                    {{ __('Login') }}
-                </a>
-             </div>
+            
             </div>
         </div>
     </div>
