1f59207152852e87d1aea301ab518870a35ffefa
SHakil Hossain <shakil@newgen-bd.com>
2024-01-31 12:12:50 +0600

    Update DocumentController.php

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -3,62 +3,60 @@
 namespace App\Http\Controllers;
 
 use App\Models\Document;
-use App\Models\User;
 use Illuminate\Http\Request;
 
 class DocumentController extends Controller
 {
 
     public function __construct()
-    {
-        // Apply 'role' middleware to specific actions
+{
+    // Apply 'role' middleware to specific actions
 
+   
 
-
-        // if (auth()->user() && in_array(auth()->user()->role->name, $roles)) {
-        //    'dsfsdfsdfdsfsdfsdf';
-        // }
-        //$this->middleware('role:superadmin')->only(['create', 'store', 'edit', 'update', 'destroy']);
-    }
+    // if (auth()->user() && in_array(auth()->user()->role->name, $roles)) {
+    //    'dsfsdfsdfdsfsdfsdf';
+    // }
+    //$this->middleware('role:superadmin')->only(['create', 'store', 'edit', 'update', 'destroy']);
+}
 
     public function index(Request $request)
     {
-
+       
         $search = $request->input('search');
-
-        $documents = Document::with('user')->when($search, function ($query) use ($search) {
+        $data=Document::count();
+        $documents = Document::when($search, function ($query) use ($search) {
             $query->where('title', 'like', '%' . $search . '%')
-                ->orWhere('tag', 'like', '%' . $search . '%')
-                ->orWhere('status', 'like', '%' . $search . '%')
-                ->orWhere('file_path', 'like', '%' . $search . '%')
-                ->orWhere('description', 'like', '%' . $search . '%');
+                  ->orWhere('tag', 'like', '%' . $search . '%')
+                  ->orWhere('status', 'like', '%' . $search . '%')
+                  ->orWhere('file_path', 'like', '%' . $search . '%')
+                  ->orWhere('description', 'like', '%' . $search . '%');
         })->paginate();
-
-        return view('document.index', compact('documents', 'search'))
+    
+        return view('document.index', compact('documents', 'search','data'))
             ->with('i', (request()->input('page', 1) - 1) * $documents->perPage());
     }
+    
 
-
-
+  
     public function create()
     {
         $document = new Document();
-        $users = User::all();
-        return view('document.create', compact('document', 'users'));
+        return view('document.create', compact('document'));
     }
 
     public function store(Request $request)
     {
         $request->validate([
-            'title' => 'required|unique:documents,title',
+            'title' => 'required',
             'status' => 'required',
             'file_path' => 'required|file|mimes:pdf,doc,docx', // Adjust allowed file types as needed
         ]);
-
+    
         // Handle file upload
         $file = $request->file('file_path');
         $filePath = $file->store('public/document'); // 'documents' is the storage folder where the file will be saved
-
+    
         // Create a new document with the validated data and file path
         $document = Document::create([
             'title' => $request->input('title'),
@@ -68,10 +66,10 @@ class DocumentController extends Controller
             'file_path' => $filePath, // Store the file path in the database
             'description' => $request->input('description'),
         ]);
-
+    
         return redirect()->route('document.index')->with('success', 'Document created successfully.');
     }
-
+    
 
     public function show($id)
     {
@@ -83,8 +81,8 @@ class DocumentController extends Controller
     public function edit($id)
     {
         $document = Document::find($id);
-        $users = User::all();
-        return view('document.edit', compact('document', 'users'));
+
+        return view('document.edit', compact('document'));
     }
 
 
@@ -95,7 +93,7 @@ class DocumentController extends Controller
             'status' => 'required',
             'file_path' => 'nullable|file|mimes:pdf,doc,docx', // Adjust allowed file types as needed
         ]);
-
+    
         // Update other fields
         $document->update([
             'title' => $request->input('title'),
@@ -104,22 +102,22 @@ class DocumentController extends Controller
             'status' => $request->input('status'),
             'description' => $request->input('description'),
         ]);
-
+    
         // Handle file upload if a new file is provided
         if ($request->hasFile('file_path')) {
             // Delete the previous file if it exists
             // Assuming you have a method in your Document model to handle file deletion
             $document->deleteFile(); // Implement this method in your Document model
-
+    
             // Handle the new file upload
             $file = $request->file('file_path');
             $filePath = $file->store('public/document'); // 'documents' is the storage folder where the file will be saved
             $document->update(['file_path' => $filePath]); // Update the file path in the database
         }
-
+    
         return redirect()->route('document.index')->with('success', 'Document updated successfully');
     }
-
+    
 
 
     public function destroy($id)
